c6069a322004a8a7c0ed3ff2dfa1022dddae3ab6
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-08 15:56:05 +0530

    [Harshal]Add: Roll Die Feature Added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,10 +5,11 @@ namespace SnakeAndLadder
     class Program
     {
         static void Main(string[] args)
-        { 
-        SnakeAndLadderGame game = new SnakeAndLadderGame();
-            Console.WriteLine("Player Start position 0 ");
+        {
+            SnakeAndLadderGame game = new SnakeAndLadderGame();
+            Console.WriteLine(game.DieRoll());
         }
-
     }
+
+    
 }
--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -9,5 +9,13 @@ namespace SnakeAndLadder
     public class SnakeAndLadderGame
     {
         int position = 0;
+
+        public int DieRoll()
+        {
+            Random random = new Random();
+            int DiePosition = random.Next(1, 7);
+            Console.WriteLine("Die Number is: " + DiePosition);
+            return DiePosition ;
+        }
     }
 }
