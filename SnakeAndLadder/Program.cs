5b15d02da4fd055aba22057b8a19e355e75036d4
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-04-08 15:31:17 +0530

    [Harshal]Add: Single Player With Position Zero

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        SnakeAndLadderGame game = new SnakeAndLadderGame();
+            Console.WriteLine("Player Start position 0 ");
+        }
+
+    }
+}
